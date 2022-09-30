aa27c2acb37ff709e9b4f146e6c1caab74402186
ihaveapatricklmao <epicgamer90.moments@gmail.com>
2022-09-30 14:28:54 -0400

    added status effect variables and a new enemy
    poison and superpoison and the hawkeye

--- python_text_game_files/python_text_game.py
+++ python_text_game_files/python_text_game.py
@@ -9,12 +9,16 @@ import time
 import turtle
 
 # global variable definitions
+Poison = random.randint(1, 3)
+SuperPoison = random.randint(4, 7)
+
 Duck = {"Name": "Duck", "MaxHP": 5, "HP": 5,  "Damage": random.randint(1,6)}
 Crab = {"Name": "Crab", "MaxHP": 10, "HP": 10, "Damage": random.randint(1, 3)}
-Bee = {"Name": "Bee", "MaxHP": 4, "HP": 4, "Damage": random.randint(1, 2)}
-Trout = {"Name": "Trout" "MaxHP": 7, "HP": 7, "Damage": random.randint(2, 5)}
+Bee = {"Name": "Bee", "MaxHP": 4, "HP": 4, "Damage": random.randint(1, 2), "StatusFx": Poison}
+Trout = {"Name": "Trout", "MaxHP": 7, "HP": 7, "Damage": random.randint(2, 5)}
+HawkEye = {"Name": "Hawk Eye", "MaxHP": 7, "HP": 7, "Damage": random.randint(3, 4)}
 
-Enemies = [Duck, Crab, Bee, Trout]
+Enemies = [Duck, Crab, Bee, Trout, HawkEye]
 PlayerHP = 20
 # class definitions
 class Entity:
