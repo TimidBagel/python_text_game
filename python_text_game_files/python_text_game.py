79307d3751404027bd6ea80d7324390e41015a29
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2022-09-30 19:48:04 -0400

    Made basic enemy stuff
    Variable even change! No death yet tho and the gameplay doesnt loop

--- python_text_game_files/python_text_game.py
+++ python_text_game_files/python_text_game.py
@@ -50,30 +50,42 @@ print(player.health)
 
 #Attack Actions ~Kit
 #This method probably sucks someone find a better version ~Kit
-def Hit():
-    return random.randint(2, 5)
-def Hunker():
-    return random.randint(2,5)
-CrabAI = [Hit(), Hunker()]
+def HitLow():
+    return {"Hurt": random.randint(1,3)}
+def HealLow():
+    return {"Heal": random.randint(2,4)}
+CrabAI = [HitLow(), HealLow()]
 
 player = Player({"health": 20, "stamina": 10, "strength": 10})
 print(player.health)
 
 Crab = Enemy({"health": 15, "Name": "Crab", "AI": CrabAI})
-Enemies.append(i)
+Enemies.append(Crab)
 
 
-print(NormalFighterAI[0]);
+
 # input validation
 
 # inventory
 
 # action loop
-#Im going to work on this ~Cormac
+
 # game loop
+
+#I am very very sorry for my spaghetti code ~Kit
 def Combat():
     chosenEnemy = Enemies[random.randrange(len(Enemies))]
     print("You encountered a "+ chosenEnemy.Name+ " it has " + str(chosenEnemy.health) + " Health\n")
+    Inp = input("What would you like to do?\n1. Strike \n2. Block\n")
+    EnemAct = random.choice(chosenEnemy.AI)
+    #Sorry about the constant if checks but I couldn't see a way around it ~Kit
+    if "Hurt" in EnemAct:
+         print(chosenEnemy.Name +" attacked you for " + str(EnemAct["Hurt"]) + " Damage\n")
+         player.health -= EnemAct["Hurt"]
+    if "Heal" in EnemAct:
+        print(chosenEnemy.Name +" healed for " + str(EnemAct["Heal"]) + " Damage. It now has " + str(chosenEnemy.health) + " Health")
+        #We will need to also add max HP so healing enemies don't gain copius amounts of health. (Same for the player)
+        chosenEnemy.health += EnemAct["Heal"]
     
 
 Combat()
